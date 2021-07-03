0d0238a76ee9d423e8d19cd8e642ddb94e9da9de
Viresh <vireshfegade@gmail.com>
2021-07-03 15:15:06 +0530

    Initialize Git repository

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask,jsonify
+import json
+
+app = Flask(__name__)
+
+f = open('data.json')
+data =json.load(f)
+
+
+@app.route('/')
+def allElements():
+    elements = []
+    for element in data:
+        elements.append(element)
+
+    return jsonify(elements)
+
+# if __name__ == "__main__":
+#     app.run(debug=False)
