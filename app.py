88192f4fda3ba8ff97ff78d3ffe286eecf574796
Viresh <vireshfegade@gmail.com>
2021-07-05 12:47:59 +0530

    update response

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def index():
 
     resp = Response(js, status=200, mimetype='application/json')
     resp.headers["Access-Control-Allow-Origin"] = "*"
-    resp.headers["Access-Control-Allow-Credentials"] = True
+    resp.headers["Access-Control-Allow-Credentials"] = "true"
     return resp
     
 
