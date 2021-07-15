fa80f864081b8eb5222b48347264b733cdeea5ab
Viresh <vireshfegade@gmail.com>
2021-07-15 23:04:59 +0530

    update data and add more endpoints

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import re
-from flask import Flask,jsonify,Response,request
+from flask import Flask, jsonify, Response, request
 import json
 
 app = Flask(__name__)
 
 f = open('data.json')
-data =json.load(f)
+data = json.load(f)
+
 
 def makeResponse(js):
     resp = Response(js, status=200, mimetype='application/json')
@@ -13,37 +14,33 @@ def makeResponse(js):
     resp.headers["Access-Control-Allow-Credentials"] = "true"
     resp.headers["Access-Control-Allow-Methods"] = "GET"
     return resp
-    
-@app.route('/')
+
+
+@app.route('/elements')
 def allElements():
-   
-        elements = []
+
+    elements = []
+    for element in data:
+        elements.append(element)
+
+    js = json.dumps(elements)
+    resp = makeResponse(js)
+    return resp
+
+
+@app.route("/elements/block/<string:blockName>")
+def block(blockName):
+    elements = []
+    blocks = ["s", "d", "p", "f"]
+
+    if blockName in blocks:
         for element in data:
-            elements.append(element)
+            if element['block'] == blockName:
+                elements.append(element)
 
         js = json.dumps(elements)
         resp = makeResponse(js)
         return resp
-        
-@app.route("/block")
-def block():
-    elements = []
-    blocks = ["s","d","p","f"]
-    if "block" in request.args:
-        blockName = request.args['block']
-        if blockName in blocks:
-            for element in data:
-                if element['block'] == blockName:
-                    elements.append(element)
-
-            js = json.dumps(elements)
-            resp = makeResponse(js)
-            return resp
-        else:
-            message = "data does not exist"
-            js = json.dumps(message)
-            resp = makeResponse(js)
-            return resp
     else:
         message = "data does not exist"
         js = json.dumps(message)
@@ -51,9 +48,179 @@ def block():
         return resp
 
 
+@app.route("/elements/state/<string:state>")
+def state(state):
+    elements = []
+    states = ['liquid', 'unknown', 'solid', 'gas']
+
+    if state in states:
+        for element in data:
+            if element['standardState'] == state:
+                elements.append(element)
+
+        js = json.dumps(elements)
+        resp = makeResponse(js)
+        return resp
+    else:
+        message = "data does not exist"
+        js = json.dumps(message)
+        resp = makeResponse(js)
+        return resp
 
 
+@app.route("/element/atomicNumber/<int:ano>")
+def atomicNumber(ano):
+    elements = []
 
+    if ano in range(1, 119):
+        for element in data:
+            if element['atomicNumber'] == ano:
+                elements.append(element)
+                break
+        js = json.dumps(elements)
+        resp = makeResponse(js)
+        return resp
+
+    else:
+        message = "data does not exist"
+        js = json.dumps(message)
+        resp = makeResponse(js)
+        return resp
+
+
+@app.route("/element/atomicName/<string:aname>")
+def atomicName(aname):
+    elements = []
+
+    for element in data:
+        if element['name'].lower() == aname.lower():
+            elements.append(element)
+            break
+    else:
+        message = "data does not exist"
+        js = json.dumps(message)
+        resp = makeResponse(js)
+        return resp
+
+    js = json.dumps(elements)
+    resp = makeResponse(js)
+    return resp
+
+
+@app.route("/element/symbol/<string:symbol>")
+def symbol(symbol):
+    elements = []
+
+    for element in data:
+        if element['symbol'] == symbol:
+            elements.append(element)
+            break
+    else:
+        message = "data does not exist"
+        js = json.dumps(message)
+        resp = makeResponse(js)
+        return resp
+    js = json.dumps(elements)
+    resp = makeResponse(js)
+    return resp
+
+
+@app.route("/element/electronicConfiguration/<int:ano>")
+def electronicConfiguration(ano):
+
+    for element in data:
+        if element['atomicNumber'] == ano:
+            element = {"configuration": element['electronicConfiguration']}
+            break
+    else:
+        message = "data does not exist"
+        js = json.dumps(message)
+        resp = makeResponse(js)
+        return resp
+    js = json.dumps(element)
+    resp = makeResponse(js)
+    return resp
+
+
+@app.route("/elements/bondingType/<string:bType>")
+def bondingType(bType):
+    elements = []
+    bondingTypes = ['unknown', 'metallic',
+                    'atomic', 'covalent network', 'diatomic']
+
+    if bType in bondingTypes:
+        for element in data:
+            if element['bondingType'] == bType:
+                elements.append(element)
+
+        js = json.dumps(elements)
+        resp = makeResponse(js)
+        return resp
+    else:
+        message = "data does not exist"
+        js = json.dumps(message)
+        resp = makeResponse(js)
+        return resp
+
+
+@app.route("/elements/type/<string:type>")
+def groupBlock(type):
+    elements = []
+    groupBlocks = ['metal', 'alkali metal', 'actinoid', 'halogen', 'alkaline earth metal',
+                   'lanthanoid', 'metalloid', 'nonmetal', 'transition metal', 'noble gas', 'post-transition metal']
+
+    if type in groupBlocks:
+        for element in data:
+            if element['groupBlock'] == type:
+                elements.append(element)
+
+        js = json.dumps(elements)
+        resp = makeResponse(js)
+        return resp
+    else:
+        message = "data does not exist"
+        js = json.dumps(message)
+        resp = makeResponse(js)
+        return resp
+
+
+@app.route("/elements/group/<int:no>")
+def group(no):
+    elements = []
+
+    if no in range(1, 19):
+        for element in data:
+            if element['group'] == no:
+                elements.append(element)
+        
+        js = json.dumps(elements)
+        resp = makeResponse(js)
+        return resp
+
+    else:
+        message = "data does not exist"
+        js = json.dumps(message)
+        resp = makeResponse(js)
+        return resp
+
+@app.route("/elements/period/<int:no>")
+def period(no):
+    elements = []
+
+    if no in range(1, 8):
+        for element in data:
+            if element['period'] == no:
+                elements.append(element)
+        
+        js = json.dumps(elements)
+        resp = makeResponse(js)
+        return resp
+
+    else:
+        message = "data does not exist"
+        js = json.dumps(message)
+        resp = makeResponse(js)
+        return resp
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
